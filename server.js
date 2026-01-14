faf701f8201c3f85d8fc94b058aa91011a0fef7c
aadya28 <aadya28.srivastava@gmail.com>
2026-01-14 18:18:59 +0530

    Fix CORS origin URL - remove trailing slash

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const io = new Server(server, {
     cors: {
         origin: [
             'http://localhost:3000',
-            'https://othelloduckgame.vercel.app/'
+            'https://othelloduckgame.vercel.app'
         ],
         methods: ["GET", "POST"]
     }
